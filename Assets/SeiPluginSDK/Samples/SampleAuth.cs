1374cecff6407e20fbff43bd6a2b89efbd34ee69
MainIsTechLabs <istechlabs7@gmail.com>
2024-10-01 21:46:26 +0300

    update samples

---
+++ Assets/SeiPluginSDK/Samples/SampleAuth.cs
@@ -0,0 +1,26 @@
+using System.Threading.Tasks;
+using TMPro;
+using UnityEngine;
+
+namespace SeiSDK
+{
+    public class SampleAuth : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI _testText;
+
+        public async void Login()
+        {
+            var task = SeiSdkManager.Instance.LoginCompass();
+
+            while (!task.IsCompleted)
+            {
+                await Task.Yield();
+            }
+
+            if (task.IsCompletedSuccessfully)
+            {
+                _testText.text = task.Result.WalletAddress;
+            }
+        }
+    }
+}
